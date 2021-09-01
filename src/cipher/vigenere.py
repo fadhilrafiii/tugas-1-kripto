bf0254ef0e0bf08350729ee7cc2dbf1a96aa5d77
fadhilrafiii <fadhilrafi90@gmail.com>
2021-09-01 12:50:43 +0700

    refactor Vigenere variant and add Playfair Algo

--- src/cipher/vigenere.py
+++ src/cipher/vigenere.py
@@ -1,40 +1,64 @@
+import random
 from collections import defaultdict
 
 class Vigenere:
-  def __init__(self, data, key):
+  def __init__(self, data, key, variant = 'standard'):
+    self.variant = variant
     self.data =  data
     self.key = key
-    self.table = [chr(i) for i in range(ord('a'),ord('z') + 1)]
+    self.key_stream = ''
+    self.conversion = ''
+
+    self.generate_keystream()
+    self.generate_conversion()
+
+  def generate_conversion(self):
+    if (self.variant == 'full'):
+      self.conversion = bytearray(random.sample(range(97, 123), 26)).decode('utf-8')
+    elif (self.variant == 'extended'):
+      self.conversion = ''.join([chr(i) for i in range(0, 256)])
+    else:
+      self.conversion = ''.join([chr(i) for i in range(97, 123)])
 
   def generate_keystream(self):
-    key_stream = ''
-    for i in range(len(self.data)):
-      key_stream += self.key[i % len(self.key)]
-
-    return key_stream
+    if (self.variant == 'autokey'):
+      for i in range(len(self.data)):
+        if (i < len(self.key)):
+          self.key_stream += self.key[i]
+        else:
+          self.key_stream += self.data[i]
+    else:
+      for i in range(len(self.data)):
+        self.key_stream += self.key[i % len(self.key)]
 
   def encrypt_val(self, origin, key):
-    return self.table[(ord(origin) + ord(key) - 194) % 26]
+    if (self.variant == 'full'):
+      return self.conversion[(self.conversion.index(origin) + self.conversion.index(key)) % 26]
+    if (self.variant == 'extended'):
+      return self.conversion[(ord(origin) + ord(key)) % 256]
+
+    return self.conversion[(ord(origin) + ord(key) - 194) % 26]
   
   def decrypt_val(self, value, key):
-    return self.table[(ord(value) - ord(key)) % 26]
+    if (self.variant == 'full'):
+      return self.conversion[(self.conversion.index(value) - self.conversion.index(key)) % 26]
+    if (self.variant == 'extended'):
+      return self.conversion[(ord(value) - ord(key)) % 256]
+
+    return self.conversion[(ord(value) - ord(key)) % 26]
 
   def encrypt(self):
-    key_stream = self.generate_keystream()
-
     encrypted = ''
     for i in range(len(self.data)):
-      encrypted += self.encrypt_val(self.data[i], key_stream[i])
+      encrypted += self.encrypt_val(self.data[i], self.key_stream[i])
 
     self.data = encrypted
     return self.data
 
   def decrypt(self):
-    key_stream = self.generate_keystream()
-
     decrypted = ''
     for i in range(len(self.data)):
-      decrypted += self.decrypt_val(self.data[i], key_stream[i])
+      decrypted += self.decrypt_val(self.data[i], self.key_stream[i])
     
     self.data = decrypted
     return self.data
--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 # Import utils
-import random
 from utils.file import read_file, write_file
 from utils.string import extract_alphabet, parse_n_char
 
 # Import Algorithm
 from cipher.vigenere import Vigenere
-from cipher.full_vigenere import FullVigenere
-from cipher.autokey_vigenere import AutokeyVigenere
 
-data = input("Masukkan data: ")
-key = input("Masukkan kunci: ")
+data = 'attack at dawn'
+key = 'lemon'
 
-cipher = AutokeyVigenere(extract_alphabet(data), key)
+cipher = Vigenere(extract_alphabet(data), key, 'extended')
+print(cipher.conversion)
+print(cipher.key_stream)
 # decryption = cipher.decrypt()
 print(f'Encrypt: {cipher.encrypt()}')
 print(f'Decrypt: {cipher.decrypt()}')

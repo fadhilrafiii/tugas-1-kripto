30f4c5bf6c180500e940baecc4aa2a4a8c570704
fadhilrafiii <fadhilrafi90@gmail.com>
2021-09-25 20:21:51 +0700

    add key in menu list, remove unused code

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -17,13 +17,12 @@ const MenuList = [
 
 const Navbar = (props) => {
   const [openNav, setOpenNav] = useState(false);
-  console.log(window.location.pathname)
   return (
     <header className="header">
       <div className="title">Cipher Algorithm</div>
       <div className={openNav ? "menu-list" : "menu-list hidden"}>
         {MenuList.map(({ route, text }) => (
-          <a href={route} className={route === window.location.pathname ? 'active' : ''}>{text}</a>
+          <a key={route} href={route} className={route === window.location.pathname ? 'active' : ''}>{text}</a>
         ))}
       </div>
       <div className="hamburger-btn" onClick={() => setOpenNav(prev => !prev)}>
